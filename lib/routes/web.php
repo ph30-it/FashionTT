44cf7fc5c010897788c183b1e23054c7342a8fea
thuongchul1304 <thuongnnpd02383@fpt.edu.vn>
2019-03-12 14:11:48 +0700

    login

--- lib/routes/web.php
+++ lib/routes/web.php
@@ -10,10 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/','HomeController@index')->name('list-product');
+Route::get('/','HomeController@index')->name('index');
+Route::get('index','HomeController@index')->name('index');
 Route::get('shop','HomeController@shop')->name('shop');
 Route::get('checkout','HomeController@checkout')->name('checkout');
 Route::get('payment','HomeController@payment')->name('payment');
 Route::get('single','HomeController@single')->name('single');
-Route::view('/welcome', 'frontend.contact');
-Route::view('/welcome', 'frontend.about');
+Route::view('contact', 'frontend.contact');
+Route::view('about', 'frontend.about');
+Route::get('login','admin\LoginController@getLogin')->name('login');
+Route::POST('login','admin\LoginController@postLogin')->name('logined');
+
+Route::get('logout','admin\LoginController@getLogout')->name('logout');
