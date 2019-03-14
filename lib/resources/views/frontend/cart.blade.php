7269f9fe13247bddc81bbf2f19284f7fe07269d2
thuongchul1304 <thuongnnpd02383@fpt.edu.vn>
2019-03-14 21:58:24 +0700

    a

--- lib/resources/views/frontend/cart.blade.php
+++ lib/resources/views/frontend/cart.blade.php
@@ -20,13 +20,13 @@
 									<ul class="sbmincart-attributes"></ul>            
 								</div>            
 								<div class="sbmincart-details-quantity">                
-									<input class="sbmincart-quantity" disabled type="text"  value="{{$val['qty']}}" autocomplete="off">
+									<input class="sbmincart-quantity" type="text" data-id="{{$val['id']}}" value="{{$val['qty']}}" autocomplete="off" max="5">
 								</div>            
 								<div class="sbmincart-details-remove">                
 									<button type="button" class="sbmincart-remove remove" data-sbmincart-idx="{{$val['id']}}">×</button>            
 								</div>            
 								<div class="sbmincart-details-price">                
-									<span class="sbmincart-price">${{$val['price']}}</span>            
+									<span class="sbmincart-price">${{$val['price']*$val['qty']}}</span>            
 								</div>           
 
 							</li>   
@@ -36,7 +36,7 @@
 						<div class="sbmincart-footer">                    
 							<div class="sbmincart-subtotal"><p class="totalsub">Subtotal: ${{$tongtien}} USD</p>
 							</div>            
-							<a href="" class="sbmincart-submit" type="submit" data-sbmincart-alt="undefined">Check Out
+							<a href="{{route('checkout')}}" class="sbmincart-submit" type="submit" data-sbmincart-alt="undefined">Check Out
 							</a>            
 						</div>   
 						<?php else: ?> 
