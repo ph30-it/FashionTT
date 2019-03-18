f4646e4b80109f1c2d1eaa5d09a3884343ad2209
thuongchul1304 <thuongnnpd02383@fpt.edu.vn>
2019-03-18 17:57:16 +0700

    comment

--- lib/app/Http/Controllers/HomeController.php
+++ lib/app/Http/Controllers/HomeController.php
@@ -14,55 +14,51 @@ class HomeController extends Controller
     {
      $this->middleware('guest')->except('logout');
  }
- public function index()
- {
-     return view('frontend.index');
- }
- public function shop(Request $request)
- {
-    if (request()->cate) {
-        $product=Product::where('category_id',request()->cate)->Paginate(12);
-            //dd($product);
-    }else{
-       $product=Product::with('images')->Paginate(12);               
+   public function index()
+   {
+       return view('frontend.index');
    }
-   $view=Product::orderBy('view', 'desc')
-   ->take(5)
-   ->get();
-   if ($request->ajax()) {
+   public function shop(Request $request)
+   {
+         $product=Product::with('images')->Paginate(12);
+        if ($request->ajax()) {
+         return view('frontend.products',['product' => $product])->render();
+     }
+     $view=Product::orderBy('view', 'desc')
+     ->take(5)
+     ->get();
+     $category=Category::all()->toArray();
+     
+     return view('frontend.shop',compact('product','view','category'));
+  }
 
-       return view('frontend.products', ['product' => $product])->render();  
-   }
-   $category=Category::all()->toArray();
-   return view('frontend.shop',compact('product','view','category'));
-}
-
-public function single($id)
-{
-    $comment=Comment::with('user')->where('product_id',$id)->get()->toArray();
-    $view=Product::orderBy('view', 'desc')
-    ->take(4)
-    ->get();
-    $single=Product::with('Images')->where('id',$id)->first()->toArray();
-                    //dd($single);
-                    //dd($single);
-    return view('frontend.single',compact('view','single','comment'));
-}
-public function comment(CommentRequest $request,$id,$uid)
-{
-    $comment = new Comment;
-    $comment->content = $request->Message;
-    $comment->user_id=$uid;
-    $comment->product_id= $id;
-    $comment->save();
-    return redirect()->back();
-}
-public function contact()
-{
-    return view('frontend.contact');
-}
-public function about()
-{
-    return view('frontend.about');
-}
+  public function single($id)
+  {
+      $comment=Comment::with('user')->where('product_id',$id)->get()->toArray();
+      $view=Product::orderBy('view', 'desc')
+      ->take(4)
+      ->get();
+      $single=Product::with('Images')->where('id',$id)->first()->toArray();
+                      //dd($single);
+                      //dd($single);
+      return view('frontend.single',compact('view','single','comment'));
+  }
+  public function comment(CommentRequest $request)
+  {
+      $comment = new Comment;
+      $comment->content = $request->content;
+      $comment->user_id=$request->user_id;
+      $comment->product_id= $request->id;
+      $comment->save();
+      $data=$comment=Comment::with('user')->where('product_id',$request->id)->get()->toArray();
+      echo json_encode($data);
+  }
+  public function contact()
+  {
+      return view('frontend.contact');
+  }
+  public function about()
+  {
+      return view('frontend.about');
+  }
 }
--- lib/resources/views/frontend/single.blade.php
+++ lib/resources/views/frontend/single.blade.php
@@ -12,45 +12,45 @@
 <div class="banner_top innerpage" id="home">
 	@include('frontend.menu')
 	<div class="clearfix"></div>
-<div class="ads-grid_shop">
-	<div class="shop_inner_inf">
-		<div class="col-md-4 single-right-left ">
-			<div class="grid images_3_of_2">
-				<div class="flexslider">
+	<div class="ads-grid_shop">
+		<div class="shop_inner_inf">
+			<div class="col-md-4 single-right-left ">
+				<div class="grid images_3_of_2">
+					<div class="flexslider">
 
-					<ul class="slides">
-						@foreach ($single['images'] as  $val)
-						<li data-thumb="{{asset('lib/public/images_product')}}/{{$val['name']}}">
-							<div class="thumb-image"> <img src="http://localhost/web/public/images/shoes/<?=$val['name']?>" data-imagezoom="true" class="img-responsive"> </div>
-						</li>
-						@endforeach
-					</ul>
-					<div class="clearfix"></div>
+						<ul class="slides">
+							@foreach ($single['images'] as  $val)
+							<li data-thumb="{{asset('lib/public/images_product')}}/{{$val['name']}}">
+								<div class="thumb-image"> <img src="http://localhost/web/public/images/shoes/<?=$val['name']?>" data-imagezoom="true" class="img-responsive"> </div>
+							</li>
+							@endforeach
+						</ul>
+						<div class="clearfix"></div>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="col-md-8 single-right-left simpleCart_shelfItem">
-			<h3><?=$single['name']?>
-				<p><span class="item_price">${{$single['price']}}</span>
-					<del>${{$single['sale']}}</del>
-				</p>
-				<div class="rating1">
-					<ul class="stars">
-						<li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-						<li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-						<li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
-						<li><a href="#"><i class="fa fa-star-half-o" aria-hidden="true"></i></a></li>
-						<li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i></a></li>
-					</ul>
-				</div>
-				<div class="occasional">
-					{{$single['description']}}
-					<div class="clearfix"> </div>
-				</div>
-				<div class="occasion-cart">
-					<div class="shoe single-item single_page_b">
-						<form action="" method="POST">
-							<button type="button" name="submit" value="{{$single['id']}}" class="addProduct"					
+			<div class="col-md-8 single-right-left simpleCart_shelfItem">
+				<h3><?=$single['name']?>
+					<p><span class="item_price">${{$single['price']}}</span>
+						<del>${{$single['sale']}}</del>
+					</p>
+					<div class="rating1">
+						<ul class="stars">
+							<li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-star" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-star-half-o" aria-hidden="true"></i></a></li>
+							<li><a href="#"><i class="fa fa-star-o" aria-hidden="true"></i></a></li>
+						</ul>
+					</div>
+					<div class="occasional">
+						{{$single['description']}}
+						<div class="clearfix"> </div>
+					</div>
+					<div class="occasion-cart">
+						<div class="shoe single-item single_page_b">
+							<form action="" method="POST">
+								<button type="button" name="submit" value="{{$single['id']}}" class="addProduct"					
 								style="font-size: 13px;
 								color: #fff;
 								background: #1d1d1d;
@@ -103,53 +103,54 @@
 		</div>
 		<div class="clearfix"> </div>
 		<div class="responsive_tabs">
-				<div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
-					<ul class="resp-tabs-list">
-						<li class="resp-tab-item resp-tab-active" aria-controls="tab_item-0" role="tab">Description</li>
-						<li class="resp-tab-item" aria-controls="tab_item-1" role="tab">Commnet</li>
-					</ul>
-					<div class="resp-tabs-container">
-						<!--/tab_one-->
-						<h2 class="resp-accordion resp-tab-active" role="tab" aria-controls="tab_item-0"><span class="resp-arrow"></span>Description</h2><div class="tab1 resp-tab-content resp-tab-content-active" aria-labelledby="tab_item-0" style="display:block">
+			<div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
+				<ul class="resp-tabs-list">
+					<li class="resp-tab-item resp-tab-active" aria-controls="tab_item-0" role="tab">Description</li>
+					<li class="resp-tab-item" aria-controls="tab_item-1" role="tab">Commnet</li>
+				</ul>
+				<div class="resp-tabs-container">
+					<!--/tab_one-->
+					<h2 class="resp-accordion resp-tab-active" role="tab" aria-controls="tab_item-0"><span class="resp-arrow"></span>Description</h2><div class="tab1 resp-tab-content resp-tab-content-active" aria-labelledby="tab_item-0" style="display:block">
 
-							<div class="single_page">
-								<h6>{{$single['name']}}</h6>
-								<p>{{$single['description']}}</p>
-								<p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie
-									blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt
-									ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore
-									magna aliqua.</p>
-							</div>
+						<div class="single_page">
+							<h6>{{$single['name']}}</h6>
+							<p>{{$single['description']}}</p>
+							<p class="para">Lorem ipsum dolor sit amet, consectetur adipisicing elPellentesque vehicula augue eget nisl ullamcorper, molestie
+								blandit ipsum auctor. Mauris volutpat augue dolor.Consectetur adipisicing elit, sed do eiusmod tempor incididunt
+								ut lab ore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. labore et dolore
+							magna aliqua.</p>
 						</div>
-						<!--//tab_one-->
-						<h2 class="resp-accordion" role="tab" aria-controls="tab_item-1"><span class="resp-arrow"></span>Reviews</h2><div class="tab2 resp-tab-content" aria-labelledby="tab_item-1">
+					</div>
+					<!--//tab_one-->
+					<h2 class="resp-accordion" role="tab" aria-controls="tab_item-1"><span class="resp-arrow"></span>Reviews</h2><div class="tab2 resp-tab-content" aria-labelledby="tab_item-1">
 
-							<div class="single_page">
-								<div class="bootstrap-tab-text-grids">
-									<div class="bootstrap-tab-text-grid">
-											@foreach($comment as $k => $val)
-											<p><img src="{{asset('lib/public/images/')}}/{{$val['user']['avatar']}}" alt="" width="50px"><b>{{$val['user']['username']}} : </b>{{$val['content']}}</p> {{$val['created_at']}}
-											@endforeach
-										<div class="clearfix"> </div>
-									</div>
-									@if(Auth::check())
-									<div class="add-review">
-										<h4>add a comment</h4>
-
-										<form action="{{route('comment',[$single['id'],Auth::user()->id])}}" method="POST">
-											@csrf
-											<textarea name="Message" required=""></textarea>
-											<input type="submit" value="SEND">
-										</form>
-									</div>
-									@endif
+						<div class="single_page">
+							<div class="bootstrap-tab-text-grids">
+								<div class="bootstrap-tab-text-grid" style="height:500px;overflow: auto">
+									@foreach($comment as $k => $val)
+									<p><img src="{{asset('lib/public/images/')}}/{{$val['user']['avatar']}}" alt="" width="50px"><b>{{$val['user']['username']}} : </b>{{$val['content']}}</p>
+									<a  href="" title="{{$val['created_at']}}">{{\Carbon\Carbon::createFromTimeStamp(strtotime($val['created_at']))->diffforHumans()}}</a>
+									@endforeach
+									<div class="clearfix"></div>
 								</div>
+								@if(Auth::check())
+								<div class="add-review">
+									<h4>add a comment</h4>
 
+									<form action="{{route('comment')}}" id="formComment" method="POST">
+										@csrf
+										<textarea name="Message" id="message" required=""></textarea>
+										<input type="submit" value="SEND">
+									</form>
+								</div>
+								@endif
 							</div>
+
 						</div>
 					</div>
 				</div>
 			</div>
+		</div>
 		<div class="new_arrivals">
 			<h3>Featured Products</h3>
 			@foreach ($view as $key => $value)
@@ -218,7 +219,33 @@
 
 <script>
 	$(document).ready(function () {
-		$('#horizontalTab').easyResponsiveTabs({
+		$('#formComment').on('submit', function(e) {
+			var message = $('#message').val();
+			var html='';
+			console.log(message);
+			e.preventDefault();
+			$.ajax({
+				headers: {
+					'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+				},
+				type: "POST",
+				url: '{{url('comment')}}',
+				data: {
+					content:message,
+					id:{{$single['id']}},
+					user_id:{{Auth::user()->id}},
+					_token: '{!! csrf_token() !!}'
+				},
+				success: function(msg) {
+					$.each(JSON.parse(msg), function(key,value){
+						html+='<p><img src="http://localhost/FashionTT/lib/public/images/'+value.user.avatar+'" width="50px"><b>'+value.user.username+' : </b>'+value.content+'</p><a  href="" title="'+value.created_at+'">'+value.created_at+'</a>';
+					});
+					html+='<div class="clearfix"></div>';
+					$('.bootstrap-tab-text-grid').html(html);
+				}
+		});
+	});
+			$('#horizontalTab').easyResponsiveTabs({
 				type: 'default', //Types: default, vertical, accordion           
 				width: 'auto', //auto or any width like 600px
 				fit: true, // 100% fit in a container
@@ -231,36 +258,36 @@
 					$info.show();
 				}
 			});
-		$('#verticalTab').easyResponsiveTabs({
-			type: 'vertical',
-			width: 'auto',
-			fit: true
+			$('#verticalTab').easyResponsiveTabs({
+				type: 'vertical',
+				width: 'auto',
+				fit: true
+			});
 		});
-	});
-</script>
+	</script>
 
-<script>
-	$(window).load(function () {
-		$('.flexslider').flexslider({
-			animation: "slide",
-			controlNav: "thumbnails"
+	<script>
+		$(window).load(function () {
+			$('.flexslider').flexslider({
+				animation: "slide",
+				controlNav: "thumbnails"
+			});
 		});
-	});
-</script>
-<script type="text/javascript">
-	jQuery(document).ready(function ($) {
-		$(".scroll").click(function (event) {
-			event.preventDefault();
-			$('html,body').animate({
-				scrollTop: $(this.hash).offset().top
-			}, 1000);
+	</script>
+	<script type="text/javascript">
+		jQuery(document).ready(function ($) {
+			$(".scroll").click(function (event) {
+				event.preventDefault();
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1000);
+			});
 		});
-	});
-</script>
-@section('js')
-<script type="text/javascript" src="{{asset('lib/public/js/bootstrap-3.1.1.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('lib/public/js/imagezoom.js')}}"></script>
-<script type="text/javascript" src="{{asset('lib/public/js/jquery.flexslider.js')}}"></script>
-<script type="text/javascript" src="{{asset('lib/public/js/easy-responsive-tabs.js')}}"></script>
-@endsection
-@stop
+	</script>
+	@section('js')
+	<script type="text/javascript" src="{{asset('lib/public/js/bootstrap-3.1.1.min.js')}}"></script>
+	<script type="text/javascript" src="{{asset('lib/public/js/imagezoom.js')}}"></script>
+	<script type="text/javascript" src="{{asset('lib/public/js/jquery.flexslider.js')}}"></script>
+	<script type="text/javascript" src="{{asset('lib/public/js/easy-responsive-tabs.js')}}"></script>
+	@endsection
+	@stop
